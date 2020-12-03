a5a0f6be78d56b00f1a539f45b37265750cc820a
r0neko <flafyrex@gmail.com>
2020-12-03 23:05:05 +0200

    pushed code.

---
+++ osuCompanion/Companion.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+
+namespace osuCompanion
+{
+    class Companion
+    {
+        static string FILE_PATH = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "osuCompanion.txt");
+        const string NO_MAP_SELECTED = "no map selected";
+
+        private static MSN msn = new MSN();
+        
+        static private void updateFile(string d)
+        {
+            using (StreamWriter sw = new StreamWriter(FILE_PATH))
+            {
+                sw.WriteLine(string.Format("- {0} -", d));
+            }
+        }
+
+        static int Main(string[] args)
+        {
+            Console.WriteLine("osu!companion - v1.0 by r0neko");
+#if DEBUG
+            Console.WriteLine("[DEBUG BUILD]");
+#endif
+            Console.WriteLine("https://github.com/r0neko/osuCompanion");
+            Console.WriteLine();
+
+            if (!File.Exists(FILE_PATH))
+            {
+                Console.WriteLine(string.Format("=> Creating '{0}'...", FILE_PATH));
+                try
+                {
+                    File.Create(FILE_PATH);
+                    updateFile("If you see this, your streaming software was configured correctly!");
+                    Console.WriteLine(string.Format("Created a new file at {0}.\nPlease select '{0}' in your streaming software to show updates. In OBS, add a new Text component, then set it to 'Read from file'.", FILE_PATH));
+                    Console.WriteLine("Press enter when ready.");
+                    _ = Console.ReadLine();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("[x] An error has occured while creating the file.\nPlease check that you have proper rights(are you admin?) and try again.");
+                    Console.WriteLine("Press enter to exit.");
+                    _ = Console.ReadLine();
+                    return 1;
+                }
+            }
+            else
+            {
+                Console.WriteLine(string.Format("=> File checks succeded! '{0}' exists.", FILE_PATH));
+            }
+
+            updateFile(NO_MAP_SELECTED);
+
+            Console.WriteLine("=> Initialising MSN Handler...");
+            msn.init();
+            msn.MessageReceived += OnMessage;
+
+            Console.WriteLine("=> Main updating loop starts.");
+
+            while (true)
+            {
+                msn.Update();
+            }
+        }
+
+        private static void OnMessage(object sender, MSN.OsuStatus status)
+        {
+            string template = status.status == MSN.Status.Listening ? "{0} to {1} - {2}" : "{1} - {2}";
+            if (status.difficulty.Length > 0) template += "[{3}]";
+            string finalString = status.status == MSN.Status.Listening ? NO_MAP_SELECTED : string.Format(template, status.status, status.title, status.artist, status.difficulty);
+
+            Console.WriteLine("=> Status update: " + finalString);
+            updateFile(finalString);
+        }
+    }
+}
---
+++ osuCompanion/MSN.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+
+namespace osuCompanion
+{
+    class MSN
+    {
+        private const string lpClassName = "MsnMsgrUIManager";
+        private WNDCLASS lpWndClass;
+        private IntPtr m_hwnd;
+        private WndProc WndProcc;
+        private OsuStatus lastStatus;
+        public event EventHandler<OsuStatus> MessageReceived;
+
+        [DllImport("user32.dll")]
+        private static extern ushort RegisterClassW([In] ref WNDCLASS lpWndClass);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr CreateWindowExW(uint dwExStyle, [MarshalAs(UnmanagedType.LPWStr)] string lpClassName, [MarshalAs(UnmanagedType.LPWStr)] string lpWindowName, uint dwStyle, int x, int y, int nWidth, int nHeight, IntPtr hWndParent, IntPtr hMenu, IntPtr hInstance, IntPtr lpParam);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr DefWindowProcW(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        private static extern bool DestroyWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        static extern bool TranslateMessage([In] ref MSG lpMsg);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr DispatchMessage([In] ref MSG lpmsg);
+
+        [DllImport("user32.dll")]
+        static extern sbyte GetMessage(out MSG lpMsg, IntPtr hWnd, uint wMsgFilterMin,
+           uint wMsgFilterMax);
+        private delegate IntPtr WndProc(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
+
+        private struct WNDCLASS
+        {
+            public uint style;
+            public WndProc lpfnWndProc;
+            public int cbClsExtra;
+            public int cbWndExtra;
+            public IntPtr hInstance;
+            public IntPtr hIcon;
+            public IntPtr hCursor;
+            public IntPtr hbrBackground;
+            [MarshalAs(UnmanagedType.LPWStr)]
+            public string lpszMenuName;
+            [MarshalAs(UnmanagedType.LPWStr)]
+            public string lpszClassName;
+        }
+
+        private struct COPYDATASTRUCT
+        {
+            public IntPtr dwData;
+            public int cbData;
+            public IntPtr lpData;
+        }
+
+        public enum Status
+        {
+            Null,
+            Listening,
+            Editing,
+            Playing,
+            Watching
+        };
+
+        public class OsuStatus : EventArgs
+        {
+            public Status status { get; set; }
+            public String artist { get; set; }
+            public String title { get; set; }
+            public String difficulty { get; set; }
+
+        }
+
+        private IntPtr CustomWndProc(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam)
+        {
+            if (msg == 0x4a)
+            {
+                COPYDATASTRUCT copydatastruct =
+                    (COPYDATASTRUCT)Marshal.PtrToStructure(lParam, typeof(COPYDATASTRUCT));
+
+                var ptr = copydatastruct.lpData;
+                if (ptr != IntPtr.Zero)
+                {
+                    string str = Marshal.PtrToStringUni(ptr, copydatastruct.cbData / 2);
+                    string[] separator = new string[] { @"\0" };
+                    string[] sourceArray = str.Split(separator, StringSplitOptions.None);
+                    if (sourceArray.Length > 8)
+                    {
+                        for (int i = 0; i < sourceArray.Length; i++)
+                        {
+                            String status = sourceArray[3].Split(new[] { ' ' }, 2)[0];
+                            Status st = status == "Listening" ? Status.Listening
+                                        : status == "Playing" ? Status.Playing
+                                            : status == "Watching" ? Status.Watching
+                                                : status == "Editing" ? Status.Editing
+                                                    : Status.Null;
+
+                            OsuStatus stat = new OsuStatus();
+                            stat.status = st;
+                            stat.artist = sourceArray[5];
+                            stat.title = sourceArray[4];
+                            stat.difficulty = sourceArray[7];
+
+                            if (this.lastStatus == null || this.lastStatus.status != stat.status || this.lastStatus.artist != stat.artist || this.lastStatus.title != stat.title)
+                            {
+                                if (this.lastStatus == null) this.lastStatus = new OsuStatus();
+                                this.lastStatus.status = stat.status;
+                                this.lastStatus.artist = stat.artist;
+                                this.lastStatus.title = stat.title;
+                                this.lastStatus.difficulty = stat.difficulty;
+                                EventHandler<OsuStatus> handler = MessageReceived;
+                                if (handler != null)
+                                {
+                                    handler(this, stat);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return DefWindowProcW(hWnd, msg, wParam, lParam);
+        }
+
+        public void init()
+        {
+            WndProcc = CustomWndProc;
+            lpWndClass = new WNDCLASS
+            {
+                lpszClassName = lpClassName,
+                lpfnWndProc = WndProcc
+            };
+            ushort num = RegisterClassW(ref lpWndClass);
+            int num2 = Marshal.GetLastWin32Error();
+            if ((num == 0) && (num2 != 0x582))
+            {
+                throw new Exception("Could not register window class");
+            }
+            m_hwnd = CreateWindowExW(0, lpClassName, string.Empty, 0, 0, 0, 0, 0, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero);
+        }
+
+        public void Update()
+        {
+            MSG msg;
+            if (GetMessage(out msg, IntPtr.Zero, 0, 0) != 0)
+            {
+                TranslateMessage(ref msg);
+                DispatchMessage(ref msg);
+            }
+        }
+
+    }
+}
